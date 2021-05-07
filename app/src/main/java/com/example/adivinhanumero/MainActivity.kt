e40896a2d6600526e3c894653b324f0487c886d0
1703703 <diogo.saraiva12@hotmail.com>
2021-05-07 10:09:48 +0100

    Iniciar o jogo e mostrar as tentativas

--- app/src/main/java/com/example/adivinhanumero/MainActivity.kt
+++ app/src/main/java/com/example/adivinhanumero/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.adivinhanumero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private val random = Random()
+    private var numeroAdivinhar : Int = 0
+    private var jogo : Int = 0
+    private var tentativas: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        novoJogo()
+
     }
+
+    private fun novoJogo() {
+        numeroAdivinhar = random.nextInt(10) + 1
+        tentativas = 0
+        jogo++
+
+        atualizaJogoTentativas()
+    }
+
+    private fun atualizaJogoTentativas() {
+        findViewById<TextView>(R.id.textViewTentativas).text = "Tentativas: " + tentativas
+        findViewById<TextView>(R.id.textViewJogo).text = "Jogo: " + jogo
+    }
+
+
+    fun adivinhar(view: View) {}
 }
